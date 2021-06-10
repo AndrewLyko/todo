e017cf1d0402719683c355c5040ad4b39d111fc2
Andrew Lyko <marian.lyko@gmail.com>
2021-06-10 20:21:31 +0200

    add task functionality

--- model.py
+++ model.py
@@ -1,5 +1,9 @@
 from datetime import datetime
 
+from pprint import pprint as pp
+
+from helpers import generate_id
+
 todos = [
     {
         "id": 1,
@@ -7,5 +11,30 @@ todos = [
         "status": False,
         "duration": 10,
         "created_date": datetime.now()
+    },
+    {
+        "id": 5,
+        "title": "lear python",
+        "status": False,
+        "duration": 10,
+        "created_date": datetime.now()
     }
 ]
+
+
+def add_todo(tasks, title, duration=60):
+    todo = {
+        "id": generate_id(tasks),
+        "title": title,
+        "status": False,
+        "duration": duration,
+        "created_date": datetime.now()
+    }
+
+    tasks.append(todo)
+
+
+# add_todo(todos, "cut")
+# add_todo(todos, "cut")
+# add_todo(todos, "cut")
+# pp(todos)
