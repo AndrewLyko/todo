e1f679ac72eb4316c11e281e42b71f69a5b64822
Andrew Lyko <marian.lyko@gmail.com>
2021-06-10 19:04:31 +0200

    create todos model and sample data

---
+++ model.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+todos = [
+    {
+        "id": 1,
+        "title": "lear python",
+        "status": False,
+        "duration": 10,
+        "created_date": datetime.now()
+    }
+]
